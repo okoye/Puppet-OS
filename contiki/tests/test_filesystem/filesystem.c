bd10443ef5390462c7b47520e21fe776759cde75
Chuka Okoye <chuka@puppetme.com>
2011-08-05 04:53:24 -0700

    shifting apps to contiki

--- contiki/tests/test_filesystem/filesystem.c
+++ contiki/tests/test_filesystem/filesystem.c
@@ -1,31 +1,32 @@
 #include "contiki.h"
 #include "filemanager.h"
+#include <stdio.h>
 
 PROCESS(filesaveprocess, "Test File Save Process");
-AUTOSTART(&filesaveprocess);
+AUTOSTART_PROCESSES(&filesaveprocess);
 
 PROCESS_THREAD(filesaveprocess, ev, data){
   PROCESS_BEGIN();
-  char msg[10];
+  char* msg;
   unsigned int counter = 1;
   struct complex_structure
   {
     unsigned int value;
     char* string;
   };
-  msg = "Chuka.Baka"
+  msg = "ChukaBaka";
   struct complex_structure mystruct;
   mystruct.value = 2;
   mystruct.string = "Chuka";
 
   printf("Write 1 Returned %d\n",writeData("file1",msg,sizeof(char[10])));
-  printf("Write 2 Returned %d\n",writeData("file2",counter,sizeof(int)));
-  printf("Write 3 Returned %d\n",writeData("file3",mystruct,
+  printf("Write 2 Returned %d\n",writeData("file2",&counter,sizeof(int)));
+  printf("Write 3 Returned %d\n",writeData("file3",&mystruct,
                           sizeof(struct complex_structure)));
 
   msg = "";
   readData("file1",msg,sizeof(char[10]));
-  printf("Read 1 Returned %s\n"msg);
+  printf("Read 1 Returned %s\n",msg);
   counter = 0;
   readData("file2",&counter,sizeof(int));
   printf("Read 2 Returned %d\n",counter);
