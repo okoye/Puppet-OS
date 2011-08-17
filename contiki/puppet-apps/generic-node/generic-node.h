10959cdce9b5d1f307754e4f84e7b0bc5a0372c9
Chuka Okoye <chuka@puppetme.com>
2011-08-16 19:05:58 -0700

    updated some files

--- contiki/puppet-apps/generic-node/generic-node.h
+++ contiki/puppet-apps/generic-node/generic-node.h
@@ -3,7 +3,7 @@
 //Default OUTPUT_BUFFER_SIZE. Should be overridden in targets
 //for optimal size.
 #define OUTPUT_BUFFER_SIZE 500
-
+#define IEEE_802154_MAC_ADDRESS 2345678954
 /******************************************************
               Method Definitions
 *******************************************************/
